93a9c9c2e6e81bdb87f27b21e9b5be61f3109288
FranciscoNicolasBrignani <brignanifran@gmail.com>
2023-07-06 16:08:58 -0300

    Proyecto listo

---
+++ src/main/java/com/mycompany/consersordemonedasycriptomonedas/ConsersorDeMonedasyCriptomonedas.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.consersordemonedasycriptomonedas;
+
+import igu.Conversor;
+
+/**
+ *
+ * @author fran2
+ */
+public class ConsersorDeMonedasyCriptomonedas {
+ 
+    public static void main(String[] args) {
+        
+        Conversor conv = new Conversor();
+        conv.setVisible(true);
+        conv.setLocationRelativeTo(null);
+        
+        
+    }
+}
